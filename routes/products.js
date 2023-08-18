89297e802a1ab1730bf689b6e6fc197b638fe9b4
glsunnykumar <45039183+glsunnykumar@users.noreply.github.com>
2023-08-18 16:26:03 +0530

    Adding Images To Bucket

--- routes/products.js
+++ routes/products.js
@@ -13,24 +13,103 @@ const FILE_TYPE_MAP ={
     'image/jpg':'jpg'
 }
 
-const storage = multer.diskStorage({
+
+// creating the storage variable to upload the file and providing the destination folder, 
+// if nothing is provided in the callback it will get uploaded in main directory
+
+const storage = multer.memoryStorage({
     destination: function (req, file, cb) {
-    
-      const isValid =  FILE_TYPE_MAP[file.mimetype];
-      let UploadError = new Error('invalid image type');
-      if(isValid){
-        UploadError = null;
-      }
-      cb(UploadError, 'public/upload')
-    },
-    filename: function (req, file, cb) {
-      const fileName = file.originalname.split(' ').join('-');  
-      const extension = FILE_TYPE_MAP[file.mimetype];
-      cb(null, `${fileName}-${Date.now()}.${extension}`);
+        cb(null, '')
     }
-  })
+})
+
+// below variable is define to check the type of file which is uploaded
+
+const filefilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg') {
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+    
+//       const isValid =  FILE_TYPE_MAP[file.mimetype];
+//       let UploadError = new Error('invalid image type');
+//       if(isValid){
+//         UploadError = null;
+//       }
+//       cb(UploadError, 'public/upload')
+//     },
+//     filename: function (req, file, cb) {
+//       const fileName = file.originalname.split(' ').join('-');  
+//       const extension = FILE_TYPE_MAP[file.mimetype];
+//       cb(null, `${fileName}-${Date.now()}.${extension}`);
+//     }
+//   })
   
-  const uploadOptions = multer({ storage: storage })
+ // const uploadOptions = multer({ storage: storage })
+
+
+ // defining the upload variable for the configuration of photo being uploaded
+const upload = multer({ storage: storage, fileFilter: filefilter });
+
+
+ s3 = new S3Client({
+    credentials: {
+        accessKeyId: "ASIATM5NDY7NU4E4JRFT", // store it in .env file to keep it safe
+        secretAccessKey: "33Nfv7rMLxKuyHdOQ15B57irpLFkcOnfhj31j0mt",
+        sessionToken :"IQoJb3JpZ2luX2VjEM3//////////wEaCmFwLXNvdXRoLTEiRzBFAiEAgf3j1LYkaz4Ah/B1u+nJPEAdzFKP3/x2aI+aH7Dnkr0CIEaEuNJKjIZ6HEQmNynm0Q5aP4STvQnJnQyudAPYpB9DKrwCCIb//////////wEQABoMMjMzOTAxOTAxNzg3IgxXi1iLd2MHjavTPV0qkAJ5Xkox00MVLURH330WReJBBndDeBczhCfKACXZictUkop0Rl1zHMA++2nKWkQGwffzXfeGJZXv+KDQE+TalOa7t30otsuY7rxCeRdEfyIly7ivh9Fady7ymIqFfGI1V9xVj6JDHPpDK+aNgtAl6W+VWq04jijNYjrK/kzXInNaKD/KQ8NcRoGN2Px7V2l+XLZq7sAs+sPKdTc+PXS2c8pfXQbO2GL9oAVj1hmqPt9SPE5h0wgosBAaiNZbjlauh/ny+IiglAzRon8x0KdpyClrCX4L5MI9nPySj79rTDm4rvq3n9j84CcHF/DpDlu++Ql9JOBL+RpXq8uQJu30D/4Gk0XPtcyE6RR6Tr8y6CcFGzDj8/umBjqdAT5A8lnDL8/dOfeWCs4uTs75YymonbFYs1IBMFz/RLJQuS2eiGMkHmEPZaSOgRG9CsRUmLKxAU8V+7kyCRy/JkzYHOU9jre4mMrqk/nxP8pklVPabuhv0ipkQCoEfgecoNiOiO8H/l2FILjCB5IOWhDRfwbvTfQS+VlfDTfAuZjADRPsEWiU+4lsE6A2y/2p30vHFHxmEpaY/lbGlM4="
+    },
+    region: "eu-west-1" // this is the region that you select in AWS account
+})
+
+router.post(`/`,upload.single('image'), async(req, res) =>{
+    const category = await Category.findById(req.body.category);
+    const file = req.file;
+    const fileName = file.filename;    
+    if(!file) return res.status(400).send('file not found');
+    // Definning the params variable to uplaod the photo
+
+       
+    const params = {
+        Bucket:process.env.CYCLIC_BUCKET_NAME,      // bucket that we made earlier
+        Key:req.file.originalname,               // Name of the image
+        Body:req.file.buffer,                    // Body which will contain the image in buffer format
+        ACL:"public-read-write",                 // defining the permissions to get the public link
+        ContentType:"image/jpeg"                 // Necessary to define the image content-type to view the photo in the browser with the link
+    };
+     s3.upload(params,async (error,data)=>{
+        if(error){
+            res.status(500).send({"err":error})  
+        }
+    const basePath =`${req.protocol}://${req.get('host')}/public/upload/`;
+    if(!category)
+    return res.status(500).send('Invalid Category');
+
+    let product = new Product({
+        name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        image:data.Location ,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
+    })
+  
+     product = await product.save();
+     if(!product)
+     return res.status(500).send('The product cannot be created');
+
+     res.send(product);
+    })
+})
 
 router.get(`/`, async (req, res) =>{
     let filter ={};
@@ -75,47 +154,9 @@ router.get(`/get/featured/:count`, async (req, res) =>{
     res.send(products);
 })
 
-router.post(`/`,uploadOptions.single('image'), async(req, res) =>{
-    
-    const category = await Category.findById(req.body.category);
-    const file = req.file;
-    const fileName = file.filename;
-    
-    if(!file) return res.status(400).send('file not found');
-    console.log(`${req.protocol}`);
-    const basePath =`${req.protocol}://${req.get('host')}/public/upload/`;
-    if(!category)
-    return res.status(500).send('Invalid Category');
-
-    let product = new Product({
-        name: req.body.name,
-        description: req.body.description,
-        richDescription: req.body.richDescription,
-        image:`${basePath}${fileName}` ,
-        brand: req.body.brand,
-        price: req.body.price,
-        category: req.body.category,
-        countInStock: req.body.countInStock,
-        rating: req.body.rating,
-        numReviews: req.body.numReviews,
-        isFeatured: req.body.isFeatured,
-    })
-  
-     product = await product.save();
-     if(!product)
-     return res.status(500).send('The product cannot be created');
-
-     res.send(product);
-})
-
-router.put(`/:id`,uploadOptions.single('image'),async(req,res)=>{
 
 
- 
-
-
-
-
+router.put(`/:id`,upload.single('image'),async(req,res)=>{
 
     if (!mongoose.isValidObjectId(req.params.id)) {
         return res.status(400).send('Invalid Product Id');
@@ -125,24 +166,28 @@ router.put(`/:id`,uploadOptions.single('image'),async(req,res)=>{
 
     const product = await Product.findById(req.params.id);
     if (!product) return res.status(400).send('Invalid Product!');
-
-
    
     const file = req.file;
     let imagepath;
 
     if(file){
         const filename = file.filename;
-        const basePath =`${req.protocol}://${req.get('host')}/public/upload/`;
-        imagepath =  `${basePath}${filename}` ;
+        // const basePath =`${req.protocol}://${req.get('host')}/public/upload/`;
+        // imagepath =  `${basePath}${filename}` ;
+        const params = {
+            Bucket:process.env.CYCLIC_BUCKET_NAME,      // bucket that we made earlier
+            Key:req.file.originalname,               // Name of the image
+            Body:req.file.buffer,                    // Body which will contain the image in buffer format
+            ACL:"public-read-write",                 // defining the permissions to get the public link
+            ContentType:"image/jpeg"                 // Necessary to define the image content-type to view the photo in the browser with the link
+        };
+         s3.upload(params,async (error,data)=>{
+            if(error){
+                res.status(500).send({"err":error})  
+            }
+        })
     }
     else{
-        await s3.putObject({
-            Body: JSON.stringify(req.body),
-            Bucket: process.env.BUCKET,
-            Key: filename,
-          }).promise()
-        
         imagepath = product.image;
     }
 
