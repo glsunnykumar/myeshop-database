0f2fadf301281e3b1473a8c1ceaf3e79ec5be2fa
glsunnykumar <45039183+glsunnykumar@users.noreply.github.com>
2023-08-25 18:53:02 +0530

    adding s3 multer and variables

--- routes/products.js
+++ routes/products.js
@@ -9,8 +9,8 @@ const AWS =require('aws-sdk');
 
 
 const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   })
 
 const FILE_TYPE_MAP ={
@@ -52,7 +52,7 @@ const storage = multer.diskStorage({
 
     const uploadedImage =  await s3.putObject({
         Body: JSON.stringify(req.body.image),
-        Bucket: process.env.BUCKET,
+        Bucket: process.env.CYCLIC_BUCKET_NAME,
         Key: fileName,
       }).promise()
 
