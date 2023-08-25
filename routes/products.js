0b0958b90f2a8c14a964490e4bf7557f8b73fcb6
glsunnykumar <45039183+glsunnykumar@users.noreply.github.com>
2023-08-25 18:00:19 +0530

    new logs Changes

--- routes/products.js
+++ routes/products.js
@@ -42,12 +42,8 @@ const storage = multer.diskStorage({
     
     const category = await Category.findById(req.body.category);
     const file = req.file;
-    console.log(req);
+    console.log(req.file);
     const fileName = req.file.name;
-
-    
-    console.log(file);
-    
     if(!file) return res.status(400).send('file not found');
 
     // const imagePath = req.file.name;
